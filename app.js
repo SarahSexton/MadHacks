8bbb1ee06c01bf4e52af589d3cb9b6b4c2df90aa
Sarah <sarahjeannesexton@live.com>
2017-10-07 14:57:45 -0500

    "Update index.html"

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-server.get('/', restify.serveStatic({
+server.get(/^\/?.*/, restify.serveStatic({
     directory: __dirname,
     default: '/index.html'
    }));
