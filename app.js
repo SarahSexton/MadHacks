1e29f7223fd256422a2262cbd29dc4efe3642b10
Sarah <sarahjeannesexton@live.com>
2017-10-07 14:52:10 -0500

    "Update index.html"

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+   }));
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -20,8 +25,3 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
-
-server.get('/', restify.serveStatic({
-    directory: __dirname,
-    default: '/index.html'
-   }));
